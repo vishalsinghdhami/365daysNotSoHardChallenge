be33ed9eb57c077d9ac86a01cebe334c99278f39
Vishal <singhdhamivishal@gmail.com>
2024-01-04 14:19:25 +0530

    closures in js

--- 04Jan2024/closuresInJS.js
+++ 04Jan2024/closuresInJS.js
@@ -1,6 +1,10 @@
-// Closures are functions inside function and it makes a normal function stateful. Below is a simple
+// Closures are functions inside function and it makes a normal function stateful. Below is a simple,
 // closure code.
-
+/*Closures have two big advantages: -
+• It helps to create self-contained modules, if we have self-contained modules then their state 
+is also self-contained which leads to less global variables and thus better code.
+• It helps to implement encapsulation and abstraction.
+*/
 function ClosureFunction() {
   var x;
   function Increment() {
