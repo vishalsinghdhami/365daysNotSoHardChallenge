9a620da555d39a53e0132bdbabf4f1b5627ffe0a
Vishal <singhdhamivishal@gmail.com>
2024-01-05 22:14:48 +0530

    arrow function

---
+++ 5Jan2024/diffb/wArrowAndNormalFunction.js
@@ -0,0 +1,6 @@
+// Four differences between an arrow function and other functions in #JavaScript 👇
+
+// 👉An arrow function does not have a prototype object 
+// 👉An arrow function can't be used as a constructor 
+// 👉An arrow function does not have an arguments object 
+// 👉it does not have its own ‘this.’ (its own scope) it get lexically scope to parent this.
