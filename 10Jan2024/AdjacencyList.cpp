81afe6f5be174b0be75cd6fdfcdbec922161b6bb
Vishal <singhdhamivishal@gmail.com>
2024-01-10 22:57:49 +0530

    implementation of Graph

---
+++ 10Jan2024/AdjacencyList.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include  <bits/stdc++.h>
+using namespace std;
+
+int main1()
+{
+    int n, m;
+    cin >> n >> m;
+    // adjacency list for undirected graph
+    // time complexity: O(2E)
+    vector<int> adj[n+1];
+    for(int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    return 0;
+}
+int main2()
+{
+    int n, m;
+    cin >> n >> m;
+    // adjacency list for directed graph
+    // time complexity: O(E)
+    vector<int> adj[n+1];
+    for(int i = 0; i < m; i++)
+    {
+        int u, v;
+        // u —> v
+        cin >> u >> v;
+        adj[u].push_back(v);
+    }
+    return 0;
+}
